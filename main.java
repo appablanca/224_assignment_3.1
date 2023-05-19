b1ba00b263a94bd71fcdae4340336dd1a9338609
appablanca <feyzieren@gmail.com>
2023-05-19 10:47:46 +0300

    git commit -m "graph.java"

---
+++ main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N,M,w,F,K;
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        F = sc.nextInt();
+        sc.nextLine();
+        
+        for (int i = 0; i < N; i++) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
